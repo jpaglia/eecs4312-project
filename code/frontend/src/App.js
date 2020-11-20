24031a297dc53dbe186affae3939ff064a41550a
Josh Abraham <josh.m.n.abraham@gmail.com>
2020-11-19 20:51:30 -0500

    Minor cleanup

--- code/frontend/src/App.js
+++ code/frontend/src/App.js
@@ -1,11 +1,11 @@
-
-
-function App() {
-  return (
-    <div>
-      Welcome to the Frontend
-    </div>
-  );
+import React, { Component } from 'react';
+class App extends Component {
+  render() {
+    return (
+      <div>
+       Welcome to the Frontend
+      </div>
+    );
+  }
 }
-
 export default App;
