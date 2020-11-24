5d41131d2408e4711542d541527722e625ba17e9
Sam Guraya <gurayasam.1@gmail.com>
2020-11-24 02:21:03 -0500

    Added Login Endpoint And Filler Methods for Queries

---
+++ code/backend/database/db_queries.py
@@ -0,0 +1,31 @@
+def getpassword(email):
+    """
+    Input (str): email of user 
+    return (str): password of user
+    """
+    return "69"
+
+
+def getpersonType(email):
+    """
+    Input (str): email of person 
+    return (str): type of individual Parent/Secretary/Teacher/None 
+    """
+    return "Secretary"
+
+
+def getSchoolName(email):
+    """
+    Input (str): email of student/secretary
+    return (str): school name
+    """
+    return "York"
+
+
+def getListOfClasses(schoolName):
+    """
+    Input (str): name of school
+    return List[str]: List of classes taught at school
+        Example ['Math', 'English', 'Science']
+    """
+    return ["Math", "English", "Science"]
--- code/backend/endpoints/setup.py
+++ code/backend/endpoints/setup.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import Flask
 from flask import request
 from flask import jsonify
-
+from database import db_queries
 import json
 import requests
 
@@ -11,4 +11,31 @@ setup = Blueprint("setup", __name__)
 @setup.route('/getSampleInfo', methods=['GET'])
 def getSampleInfo():
   return 'Hello'
-  
+
+@setup.route('/login', methods=['POST'])
+def login():
+  """
+  Description: This endpoint checks to see if a user enters a password properly
+  Input Json Request: {
+    email, 
+    password
+  }
+  return: {
+    valid: Bool 
+    type: str -- type of user
+  }
+  """
+  req_data = request.get_json()
+  email = req_data['email'] 
+  password = req_data['password']
+
+  actual_password = db_queries.getpassword(email)
+  persontype = db_queries.getpersonType(email)
+  valid = False
+  if actual_password == password:
+    valid = True
+
+  return jsonify(
+    valid = valid,
+    type = persontype
+  )
