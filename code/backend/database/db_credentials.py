b1b2a29303a10a19b767f46e355d6ecc7bb84c7f
Amer Alshoghri <aalshoghri@gmail.com>
2020-11-25 00:43:41 -0500

    u see no credentials here

--- code/backend/database/db_credentials.py
+++ code/backend/database/db_credentials.py
@@ -1,7 +1,7 @@
 # The following credentials are used to connect to the MySQL instance on Google Cloud
 
-USERNAME = 'admin'
-PASSWORD = 'lol12345'
-HOST = 'eecs4314-schooldb-instance.ctcbke4ze9w6.us-east-1.rds.amazonaws.com'
-DB_NAME = 'schooldb1'
-PORT='3306'
+USERNAME = ''
+PASSWORD = ''
+HOST = ''
+DB_NAME = ''
+PORT=''
