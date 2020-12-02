da0ce35e0eafc6d1b54d57f51e1282d9dbc97adc
Julia Paglia <jpaglia@bell.net>
2020-12-01 20:44:40 -0500

    minor fixes

--- code/backend/endpoints/setup.py
+++ code/backend/endpoints/setup.py
@@ -415,9 +415,10 @@ def searchRecords():
   data = request.get_json()
   firstName = data['name'].split(' ')[0]
   lastName = data['name'].split(' ')[1]
+  schoolName = data['schoolName']
   recordType = data['type']
 
-  result = db_queries.recordExists(firstName, lastName, recordType)
+  result = db_queries.recordExists(firstName, lastName, recordType, schoolName)
   
   return jsonify(result)
 
